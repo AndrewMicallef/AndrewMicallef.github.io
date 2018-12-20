3503ca0cb73c3d83e9b67228891c990dadbba63b
Andrew <andrew.micallef@live.com.au>
2018-12-20 21:31:56 +1100

    fresh start

--- js/virtualrig.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var svg = InteractiveSVG.create('testing',);
-    svg.addPoint({x: 100, y: 100});
-    svg.addPoint({x: 100, y: 100, static: true});
-    svg.addStaticPoint({x: 140, y: 100});
-})();
